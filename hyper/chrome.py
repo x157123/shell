2d8c42b60f30c913a3d2265def0573c693ecabd5
liulei <x157123123@gmail.com>
2025-02-14 03:41:47 +0800

    代码提交

--- hyper/chrome.py
+++ hyper/chrome.py
@@ -245,7 +245,7 @@ def main(client, serverId, appId, decryptKey):
                 # 聚焦
                 div_el.click()
                 # 发送密钥
-                div_el.element.send_keys(decrypt_aes_ecb(decryptKey, encrypted_data_base64, 'privateKey'))
+                tab.driver.switch_to.active_element.send_keys(decrypt_aes_ecb(decryptKey, encrypted_data_base64, 'privateKey'))
                 # 确认导入
                 click_element(tab, "x://button[normalize-space()='IMPORT KEY']")
     else:
