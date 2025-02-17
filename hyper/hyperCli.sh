cf70d4cb2e7c5d7c2707358d3c275704cf0f7614
liulei <x157123123@gmail.com>
2025-02-17 23:00:33 +0800

    调整脚本

--- hyper/hyperCli.sh
+++ hyper/hyperCli.sh
@@ -58,6 +58,9 @@ if [ -z "$USER" ]; then
   echo "Warning: --user 未指定，将默认以 admin 身份执行相关操作（如需特定用户，请使用 --user）"
 fi
 
+# 安装其他插件
+pip3 install psutil requests paho-mqtt selenium pycryptodome loguru pyperclip
+
 sudo mkdir -p /opt/hyper/
 
 # 如果 /opt/hyper/hyperCli.py 存在，则先删除旧文件
