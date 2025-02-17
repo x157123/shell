bd4dcc6eadc6c36fdb39757e3e6cfe5058aba8c1
liulei <x157123123@gmail.com>
2025-02-17 13:04:15 +0800

    调整脚本

--- hyper/chrome.py
+++ hyper/chrome.py
@@ -224,6 +224,14 @@ def monitor_switch(tab, client, serverId, appId, public_key_tmp, user, display):
                 logger.info("未连接到主网络")
                 error += 1
             else:
+                # 检查页面是否响应
+                if tab.is_alive:
+                    print("页面正常")
+                else:
+                    tab.refresh()  # 重新加载页面
+                    logger.info("refresh pages:")
+                    time.sleep(5)
+
                 logger.info("已连接到主网络")
                 if first > 0:
                     # 如果是第一次连接 推送key到服务器上
