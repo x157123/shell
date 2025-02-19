361f72779d898c75762f1cf53ef96ac31342b7f4
liulei <x157123123@gmail.com>
2025-02-19 23:33:25 +0800

    tijiao

--- nexus/nexusChrom.py
+++ nexus/nexusChrom.py
@@ -351,7 +351,7 @@ def get_app_info(serverId, appId, operationType, description):
 
 
 def setup_wallet(self, key):
-    extensions = self.browser.new_tab(url="chrome:///")
+    extensions = self.browser.new_tab(url="chrome://extensions/")
     time.sleep(3)
 
     toggle_ele = (
