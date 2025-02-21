586801eb67f97884b4c493230452ab8a5d326686
liulei <x157123123@gmail.com>
2025-02-21 13:56:04 +0800

    代码提交

--- nexus/nexusChrom.sh
+++ nexus/nexusChrom.sh
@@ -436,7 +436,7 @@ fi
 export DISPLAY=:${window}
 
 echo "启动 google-chrome —— 使用远程调试模式监听 9515 端口..."
-screen -dmS chrome bash -c "export DISPLAY=:${window}; google-chrome --remote-debugging-port=9515 --no-first-run --user-data-dir=/tmp/nexus/userData/9515 --load-extension=/home/admin/extensions/chrome-cloud"
+screen -dmS chrome bash -c "export DISPLAY=:${window}; google-chrome --remote-debugging-port=9515 --no-first-run --user-data-dir=/tmp/nexus/userData/9517 --load-extension=/home/admin/extensions/chrome-cloud"
 
 EOF
 
