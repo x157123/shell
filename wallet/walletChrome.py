b3ed578031e76e9358565e74e6fa5dd930f337e5
liulei <x157123123@gmail.com>
2025-03-09 23:36:08 +0800

    调整钱包登陆

--- wallet/walletChrome.py
+++ wallet/walletChrome.py
@@ -368,6 +368,6 @@ if __name__ == '__main__':
 
     args.evm_id = '88645'
     args.evm_address = '0x9D621e237BD6342CeD54F20E0c31FdAdD268b2F8'
-    args.evm_id2 = '88101'
+    args.evm_id2 = '88104'
     test = Test()
     asyncio.run(test.run(evm_id=args.evm_id, evm_address=args.evm_address, evm_id2=args.evm_id2))
