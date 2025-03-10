ce8e79ce3a232c1bcbf0a853211b9d770e04a405
liulei <x157123123@gmail.com>
2025-03-10 16:58:59 +0800

    页面错误

--- wallet/walletChrome.py
+++ wallet/walletChrome.py
@@ -377,31 +377,30 @@ class Test(object):
 
     async def __main(self, address, wallet) -> bool:
         page = await self.__get_page()
-        try:
-            logger.info("登录钱包")
-            await asyncio.wait_for(fut=self.__login_wallet(page=page, evm_id=wallet), timeout=60)
-            logger.info("开始添加网络")
-            await asyncio.wait_for(fut=self.__add_net_work(page=page, coin_name='base'), timeout=60)
-            logger.info("连接钱包")
-            flag = await asyncio.wait_for(fut=self.__link_account(page=page), timeout=60)
-            if not flag:
-                logger.error(f'连接钱包错误 ==> {flag}')
+        # try:
+        logger.info("登录钱包")
+        await asyncio.wait_for(fut=self.__login_wallet(page=page, evm_id=wallet), timeout=60)
+        logger.info("开始添加网络")
+        await asyncio.wait_for(fut=self.__add_net_work(page=page, coin_name='base'), timeout=60)
+        logger.info("连接钱包")
+        flag = await asyncio.wait_for(fut=self.__link_account(page=page), timeout=60)
+        if not flag:
+            logger.error(f'连接钱包错误 ==> {flag}')
+            return False
+        logger.info("开始充值")
+        for key in address:
+            bool = await asyncio.wait_for(fut=self.__do_task(page=page, evm_id=key["secretKey"], evm_address=key["publicKey"]), timeout=200)
+            if bool is False:
+                logger.error(f'充值失败:停止充值 ==> {key["secretKey"]} {key["publicKey"]}')
                 return False
-            logger.info("开始充值")
-            for key in address:
-                bool = await asyncio.wait_for(fut=self.__do_task(page=page, evm_id=key["secretKey"], evm_address=key["publicKey"]), timeout=200)
-                if bool is False:
-                    logger.error(f'充值失败:停止充值 ==> {key["secretKey"]} {key["publicKey"]}')
-                    return False
-                time.sleep(2)
-                url = 'https://relay.link/bridge/base?fromChainId=8453&fromCurrency=0x0000000000000000000000000000000000000000&toCurrency=0x0000000000000000000000000000000000000000'
-                page.get(url=url)
-                time.sleep(10)
-        except Exception as error:
-            logger.error(f'error ==> {error}')
-            ...
-        finally:
-            page.quit()
+            time.sleep(2)
+            url = 'https://relay.link/bridge/base?fromChainId=8453&fromCurrency=0x0000000000000000000000000000000000000000&toCurrency=0x0000000000000000000000000000000000000000'
+            page.get(url=url)
+            time.sleep(10)
+        # except Exception as error:
+        #     logger.error(f'error ==> {error}')
+        # finally:
+        #     page.quit()
         return True
 
     async def run(self, address, wallet):
