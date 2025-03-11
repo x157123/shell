41660eab14bb7af99798b1ccffcf0f000181a159
liulei <x157123123@gmail.com>
2025-03-11 15:47:23 +0800

    连接钱包

--- gaia/gaiaChrom.py
+++ gaia/gaiaChrom.py
@@ -1046,6 +1046,8 @@ class TaskSet:
                 self.res_info = ''
                 self.tab.get(url='https://www.gaianet.ai/chat')
                 time.sleep(5)
+                self.__click_ele(page=self.tab, xpath='x://button[text()="Accept All"]')
+                time.sleep(2)
                 self.tab.wait.eles_loaded(locators='x://button[text()="Connect"]', timeout=10, any_one=True)
                 time.sleep(5)
                 self.__click_ele(page=self.tab, xpath='x://div[text()="Signma"]')
