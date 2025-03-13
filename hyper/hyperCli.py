111fbede69fccd0a8e87d1b47d4f1289ab720389
liulei <x157123123@gmail.com>
2025-03-13 12:16:08 +0800

    代码提交

--- hyper/hyperCli.py
+++ hyper/hyperCli.py
@@ -130,10 +130,10 @@ def main(client, serverId, appId, decryptKey, user, display):
             if last_lines:
                 # 查找是否网络连接失败 Sending reconnect signal
                 if check_reconnect_signal(last_lines, 'Sending reconnect signal'):
-                    logger.info("未连接网络。重新连接")
+                    logger.info(f"未连接网络。重新连接->{num}")
                     restart()
                 else:
-                    logger.info("已连接网络。")
+                    logger.info(f"已连接网络。->{num}")
 
             if num > 10:
                 num = 0
