d47b1c92885e122dd498185d16a5eaf4070f707b
liulei <x157123123@gmail.com>
2025-03-20 00:39:45 +0800

    代码提交

--- unin/unionChrom.py
+++ unin/unionChrom.py
@@ -513,13 +513,13 @@ class Test(object):
     async def run(self, union_id, user, port, key, union_address, text) -> bool:
         page = await self.__get_page(index=union_id, port=port, user=user)
         # try:
-        await asyncio.wait_for(fut=self.__do_faucet_task(page=page, evm_id=union_id, evm_address=key), timeout=100)
         await asyncio.wait_for(fut=self.setup_evm_wallet(page=page, index=union_id), timeout=100)
         await asyncio.wait_for(fut=self.open_wallet(page=page, union_address=union_address, text=text), timeout=100)
         # 激活钱包                                    
         await asyncio.wait_for(fut=self.activate_wallet(page=page, union_address=union_address), timeout=100)
+        await asyncio.wait_for(fut=self.__do_faucet_task(page=page, evm_id=union_id, evm_address=key), timeout=100)
         # await asyncio.wait_for(fut=self.get_wallet(page=page, union_address=union_address, text=text), timeout=100)
-        # await asyncio.wait_for(fut=self.__do_task(page=page, union_id=union_id, union_address=union_address), timeout=100)
+        await asyncio.wait_for(fut=self.__do_task(page=page, union_id=union_id, union_address=union_address), timeout=100)
         await self.test(page, net="Babylon Testnet")
         await self.test(page, net="Stargaze Testnet")
         await self.test(page, net="Stride Testnet")
