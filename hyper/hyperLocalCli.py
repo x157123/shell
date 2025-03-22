3b48ce1555b5519553a80d616a7e4dd119a84526
liulei <x157123123@gmail.com>
2025-03-23 00:21:12 +0800

    代码提交

--- hyper/hyperLocalCli.py
+++ hyper/hyperLocalCli.py
@@ -68,7 +68,6 @@ def start():
     install_output = run_command_blocking("curl https://download.hyper.space/api/install | bash")
     if "Installation completed successfully." not in install_output:
         logger.info("安装失败或未检测到成功提示。")
-        return
     logger.info("安装成功！")
 
     logger.info("===== 检测是否已启动 =====")
