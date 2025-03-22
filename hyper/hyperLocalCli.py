8c38315e384f09c63a45d860effa5f766457647d
liulei <x157123123@gmail.com>
2025-03-22 21:08:09 +0800

    代码提交

---
+++ hyper/hyperLocalCli.py
@@ -0,0 +1,171 @@
+import subprocess
+import time
+import re
+import argparse
+from loguru import logger
+
+def run_command_blocking(cmd, print_output=True):
+    """
+    执行命令，等待命令执行结束，并返回输出内容。
+    """
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    if print_output:
+        print(result.stdout)
+    return result.stdout
+
+
+
+def run_command_and_print(cmd, wait_for=None, print_output=True):
+    """
+    实时执行命令，打印输出。如果指定了 wait_for，当检测到该关键字时返回已收集的输出内容。
+    """
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    collected_output = ""
+    while True:
+        line = process.stdout.readline()
+        if line:
+            collected_output += line
+            if print_output:
+                print(line.strip())
+            if wait_for and wait_for in line:
+                break
+        if not line and process.poll() is not None:
+            break
+    process.wait()  # 确保进程退出
+    return collected_output
+
+
+
+def read_last_n_lines(file_path, n):
+    """读取文件最后 n 行"""
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            # 使用 collections.deque 高效读取最后 n 行
+            from collections import deque
+            lines = deque(file, maxlen=n)
+            return list(lines)
+    except FileNotFoundError:
+        print(f"错误：文件 {file_path} 未找到。")
+        return []
+    except Exception as e:
+        print(f"读取文件时发生错误：{e}")
+        return []
+
+
+def check_reconnect_signal(lines, target_string):
+    """检查指定字符串是否在行列表中"""
+    for line in lines:
+        if target_string in line:
+            return True
+    return False
+
+def start():
+    logger.info("===== 检测是否已启动 =====")
+    status_output = run_command_blocking("/root/.aios/aios-cli status")
+    if "Daemon running on" in status_output:
+        logger.info("杀掉程序。")
+        run_command_blocking("/root/.aios/aios-cli kill")
+        time.sleep(10)  # 等待 10 秒
+    logger.info("检查结束！")
+
+    # 2. 启动后端服务（后台运行，不阻塞）
+    subprocess.Popen("/root/.aios/aios-cli start", shell=True)
+    logger.info("后端命令已启动。")
+    time.sleep(5)  # 等待后端服务启动
+
+    # Hive 登录，提取 Public 和 Private Key
+    logger.info("开始 Hive 登录...")
+    run_command_and_print("/root/.aios/aios-cli hive login", wait_for="Authenticated successfully!")
+
+    # if public_key is not None:
+    #     logger.info("已配置了key...")
+    # else:
+
+    # 6. 获取key执行 hive whoami 命令
+    logger.info("执行 hive whoami 命令...")
+    login_output = run_command_blocking("/root/.aios/aios-cli hive whoami")
+    public_key = None
+    private_key = None
+    public_match = re.search(r"Public:\s*(\S+)", login_output)
+    private_match = re.search(r"Private:\s*(\S+)", login_output)
+    if public_match:
+        public_key = public_match.group(1)
+    if private_match:
+        private_key = private_match.group(1)
+
+    with open("/root/.config/hyperspace/key.pem", "r", encoding="utf-8") as file:
+        key_content = file.read()
+
+    logger.info(f"Public Key: {public_key}")
+    logger.info(f"Private Key: {private_key}")
+    # logger.info(f"key_content: {key_content}")
+    logger.info("whoami 命令执行完毕。")
+
+    # 7. 执行 hive select-tier 5 命令
+    logger.info("执行 hive select-tier 5 命令...")
+    run_command_blocking("/root/.aios/aios-cli hive select-tier 5")
+    logger.info("select-tier 命令执行完毕。")
+
+    # 8. 执行 hive connect 命令
+    logger.info("执行 hive connect 命令...")
+    run_command_blocking("/root/.aios/aios-cli hive connect")
+    logger.info("connect 命令执行完毕。")
+
+def restart():
+
+    logger.info("开始 Hive 登录...")
+    run_command_and_print("/root/.aios/aios-cli hive login", wait_for="Authenticated successfully!")
+
+    logger.info("执行 hive connect 命令...")
+    run_command_blocking("/root/.aios/aios-cli hive connect")
+    logger.info("connect 命令执行完毕。")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="获取应用信息")
+    parser.add_argument("--data", type=str, help="数据", required=True)
+    args = parser.parse_args()
+    logger.info(f"===== 启动 ====={args.data}")
+    # start()
+    # # 等待20S
+    # time.sleep(20)
+    # num = 10
+    # count = 0
+    # # 获取积分
+    # while True:
+    #     try:
+    #         num += 1
+    #         # 读取最后 3 行
+    #         last_lines = read_last_n_lines('/root/hyperCliOutput.log', 3)
+    #         if last_lines:
+    #             # 查找是否网络连接失败 Sending reconnect signal
+    #             if check_reconnect_signal(last_lines, 'Sending reconnect signal'):
+    #                 logger.info(f"未连接网络。重新连接->{num}")
+    #                 count += 1
+    #                 if count > 3:
+    #                     restart()
+    #             else:
+    #                 count = 0
+    #                 logger.info(f"已连接网络。->{num}")
+    #
+    #         if num > 10:
+    #             num = 0
+    #             logger.info("\n===== 积分查询输出 =====")
+    #             login_output = run_command_blocking("/root/.aios/aios-cli hive points")
+    #             points = None
+    #             public_match = re.search(r"Points:\s*(\S+)", login_output)
+    #             if public_match:
+    #                 points = public_match.group(1)
+    #
+    #             if not points or points == "None":
+    #                 logger.info("获取积分失败,重新启动。")
+    #                 count += 1
+    #                 if count > 3:
+    #                     restart()
+    #                     num = 7
+    #             else:
+    #                 count = 0
+    #                 logger.info(f"points: {points}")
+    #                 logger.info("获取积分完成。")
+    #         time.sleep(360)
+    #     except Exception as e:
+    #         logger.info("异常。")
