d683971da6dae7f7699ffbee4a8d65ce97823634
liulei <x157123123@gmail.com>
2025-03-22 21:51:29 +0800

    代码提交

--- hyper/hyperLocalCli.py
+++ hyper/hyperLocalCli.py
@@ -138,7 +138,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     logger.info(f"===== 启动 ====={args.data}")
     restored_data = decompress_data(args.data)
-    print("恢复后的数据：")
+    print(f"恢复后的数据：{restored_data}")
     # start()
     # # 等待20S
     # time.sleep(20)
