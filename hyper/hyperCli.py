bb7c6c3d9dbcd9d2f4b4c3498c3621aa832cfe3e
liulei <x157123123@gmail.com>
2025-03-28 16:18:16 +0800

    导入数据

--- hyper/hyperCli.py
+++ hyper/hyperCli.py
@@ -364,7 +364,7 @@ def start(client, serverId, appId, decryptKey, user, display):
     # logger.info(f"key_content: {key_content}")
     logger.info("whoami 命令执行完毕。")
     # if publicKey is None and public_key != publicKey:
-    client.publish("hyperCli", json.dumps(get_info(serverId, "hyperCli", public_key, private_key, key_content)))
+    # client.publish("hyperCli", json.dumps(get_info(serverId, "hyperCli", public_key, private_key, key_content)))
 
     # 7. 执行 hive select-tier 5 命令
     logger.info("执行 hive select-tier 5 命令...")
