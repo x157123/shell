d01147c18083b4102aa0fcc356aa06a6ae2cbcf9
liulei <x157123123@gmail.com>
2025-07-09 08:40:41 +0800

    跳过钱包安装

--- nexus_25_07_08/start.py
+++ nexus_25_07_08/start.py
@@ -87,7 +87,6 @@ async def run_remote_script(
                 if stderr:
                     logger.info(f"=== {host} STDERR ===\n{stderr}")
                 append_date_to_file("./ex_end.txt", host)
-                time.sleep(10)
         except (asyncssh.Error, OSError) as e:
             logger.error(f"[EXCEPTION] {host}: {e}")
 
