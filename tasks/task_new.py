1e4d305fb1a46a74fbf201e8efbd72d2bef85c11
liulei <x157123123@gmail.com>
2025-09-18 14:38:48 +0800

    代码提交

--- tasks/task_new.py
+++ tasks/task_new.py
@@ -996,8 +996,16 @@ def __do_airdrop(page, evm_ids):
             logger.info(f'账号登陆成功: {evm_id}')
             signma_log(message=f"登陆成功", task_name=f'airdrop_log', index=evm_id)
         else:
-            logger.info(f'账号登陆失败: {evm_id}')
-            signma_log(message=f"登陆失败", task_name=f'airdrop_log', index=evm_id)
+            __handle_signma_popup(page=page, count=0)
+            __login_wallet(page=page, evm_id=evm_id)
+            __handle_signma_popup(page=page, count=0)
+            tp = page.new_tab('chrome-extension://ohgmkpjifodfiomblclfpdhehohinlnn/popup.html')
+            if __get_ele(page=tp, xpath=f'x://span[contains(@class," account_info_label") and contains(text(), "1-{evm_id}")]'):
+                logger.info(f'账号登陆成功: {evm_id}')
+                signma_log(message=f"登陆成功", task_name=f'airdrop_log', index=evm_id)
+            else:
+                logger.info(f'账号登陆失败: {evm_id}')
+                signma_log(message=f"登陆失败", task_name=f'airdrop_log', index=evm_id)
         tp.close()
     main_page = page.new_tab('https://airdrop.0gfoundation.ai/flow')
     time.sleep(360000)
