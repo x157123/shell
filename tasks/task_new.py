f308b0ed1fcc56f79c1b7f6644823df20dae050f
liulei <x157123123@gmail.com>
2025-09-24 18:48:36 +0800

    代码提交

--- tasks/task_new.py
+++ tasks/task_new.py
@@ -3037,8 +3037,8 @@ if __name__ == '__main__':
                 _type = arg[0]
                 _id = arg[1]
 
-                # if _type == 'nexus':
-                if _type:
+                if _type == 'prismax':
+                # if _type:
                     if _type == 'gift':
                         evm_id = _id
                         evm_addr = arg[2]
@@ -3125,8 +3125,8 @@ if __name__ == '__main__':
                         _page.quit()
                     except Exception:
                         logger.exception("退出错误")
-                # if _type == 'nexus':
-                if _type:
+                if _type == 'prismax':
+                # if _type:
                     logger.info(f'数据{_end}:{_task_type}:{_task_id}')
                     if _end:
                         if _task_id and platform.system().lower() != "windows":
@@ -3136,10 +3136,11 @@ if __name__ == '__main__':
                                 _end_day_task.append(_task_id)
                     else:
                         signma_log(message=_task, task_name=f'error_task_{get_date_as_string()}', index=evm_id)
-            if len(filtered) > 24:
-                time.sleep(1000)
-            elif len(filtered) > 12:
-                time.sleep(1800)
-            else:
-                time.sleep(3600)
-        time.sleep(3600)
+            time.sleep(600)
+            # if len(filtered) > 24:
+            #     time.sleep(1000)
+            # elif len(filtered) > 12:
+            #     time.sleep(1800)
+            # else:
+            #     time.sleep(3600)
+        # time.sleep(3600)
