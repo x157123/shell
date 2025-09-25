952474f331c4fb77c6ded3775e1fbb89adcc5944
liulei <x157123123@gmail.com>
2025-09-25 23:26:37 +0800

    代码提交

--- tasks/task_new.py
+++ tasks/task_new.py
@@ -2374,6 +2374,7 @@ def __do_task_prismax(page, evm_id, evm_addr, index):
 
         sum_num_str = "0"
         if _login:
+            __bool = True
             time.sleep(2)
             sum_num_str = __get_ele_value(page=main_page, xpath='x://span[normalize-space()="All-Time Prisma Points"]/following-sibling::div/span')
             if float(sum_num_str.replace(',', '')) > 3500:
@@ -2436,7 +2437,7 @@ def __do_task_prismax(page, evm_id, evm_addr, index):
                             signma_log(message=(sum_num_str or "0").replace(",", ""), task_name=f'prismax_point_tmps_{get_date_as_string()}', index=evm_id)
     except Exception as e:
         logger.info(f"窗口{index}处理任务异常: {e}")
-    return True
+    return __bool
 
 
 def __do_task_prismax_bak(page, evm_id, evm_addr, index):
