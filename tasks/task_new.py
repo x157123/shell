03d93d327fac253d368093caff634351e268ac8e
liulei <x157123123@gmail.com>
2025-09-25 18:48:53 +0800

    代码提交

--- tasks/task_new.py
+++ tasks/task_new.py
@@ -3169,7 +3169,7 @@ if __name__ == '__main__':
                                 _end_day_task.append(_task_id)
                     else:
                         signma_log(message=_task, task_name=f'error_task_{get_date_as_string()}', index=evm_id)
-                    time.sleep(600)
+                    time.sleep(60)
             # if len(filtered) > 24:
             #     time.sleep(600)
             # elif len(filtered) > 12:
