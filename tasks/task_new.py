86b81edb32ae465a7fa1babd2e34f689ed658656
liulei <x157123123@gmail.com>
2025-10-16 08:38:23 +0800

    代码提交

--- tasks/task_new.py
+++ tasks/task_new.py
@@ -1527,6 +1527,11 @@ def __do_task_nexus_hz(page, evm_id, evm_addr, index):
             __bool_d = False
             if __get_ele(page=nexus, xpath='x://button[@data-testid="ConnectButton"]', loop=1) is None:
                 __bool_a = __do_task_nexus_hz_lq(page=page, nexus=nexus, nexus_no_bad=nexus_no_bad, _id=_idc, name='Shoulder Blaster Glyph', _evm_addr=evm_addr, _index=index, _jf=12000)
+                if __bool_a:
+                    logger.info('第一个徽章领取成功')
+                else:
+                    __bool_a = __do_task_nexus_hz_lq(page=page, nexus=nexus, nexus_no_bad=nexus_no_bad, _id=_idc, name='Shoulder Blaster Glyph', _evm_addr=evm_addr, _index=index, _jf=12000)
+                    
                 __bool_b = __do_task_nexus_hz_lq(page=page, nexus=nexus, nexus_no_bad=nexus_no_bad, _id=_idd, name='Sunset Boulevard Glyph', _evm_addr=evm_addr, _index=index, _jf=5000)
                 __bool_c = __do_task_nexus_hz_lq(page=page, nexus=nexus, nexus_no_bad=nexus_no_bad, _id=_ide, name='Eat Your Arpeggi-ohs Glyph', _evm_addr=evm_addr, _index=index, _jf=3500)
                 __bool_d = __do_task_nexus_hz_lq(page=page, nexus=nexus, nexus_no_bad=nexus_no_bad, _id=_idf, name='Boom Bap Glyph', _evm_addr=evm_addr, _index=index, _jf=3000)
