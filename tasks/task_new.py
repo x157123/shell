fcedfde5ec1ec59d89eb1423f346d3a80870be62
liulei <x157123123@gmail.com>
2025-10-31 15:30:27 +0800

    代码提交

--- tasks/task_new.py
+++ tasks/task_new.py
@@ -4236,4 +4236,4 @@ if __name__ == '__main__':
                     #     time.sleep(1200)
                     # else:
                     #     time.sleep(1800)
-        time.sleep(300)
+        time.sleep(1800)
