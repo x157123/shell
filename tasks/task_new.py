2e1c66b95e6527010d3928c5d00597f82e7e4525
liulei <x157123123@gmail.com>
2025-11-01 17:44:53 +0800

    代码提交

--- tasks/task_new.py
+++ tasks/task_new.py
@@ -2517,9 +2517,9 @@ def __do_task_nexus_join(page, evm_id, index, x_name, x_cookies):
             if not _amount or float(_amount) <= 0:
                 _amount = '0'
                 __bool = False
-                if __x_bool and not __app_join:
-                    __app_join = join(nexus, _name)
-                signma_log(message=f'{_amount},{__x_bool},{__bool},{_name},{x_name},{__app_join}', task_name=f'nexus_join_ssb', index=evm_id)
+            if __x_bool and not __app_join:
+                __app_join = join(nexus, _name)
+            signma_log(message=f'{_amount},{__x_bool},{__bool},{_name},{x_name},{__app_join}', task_name=f'nexus_join_ssb', index=evm_id)
             # if not __x_bool:
             #     __bool = True
     except Exception as e:
