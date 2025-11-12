a4cb9bd708e9b217ab4f782015e4cd6d3a5aaf65
liulei <x157123123@gmail.com>
2025-11-13 01:15:27 +0800

    代码提交

--- tasks/task_new.py
+++ tasks/task_new.py
@@ -4679,4 +4679,4 @@ if __name__ == '__main__':
                     # else:
                     #     time.sleep(random.randint(600, 1800))
                     time.sleep(random.randint(30, 60))
-        time.sleep(random.randint(60, 80))
+        time.sleep(random.randint(600, 1200))
