347703fc15f2bd8c8736bf81692b2a4ebb97bab9
liulei <x157123123@gmail.com>
2025-12-09 09:54:56 +0800

    代码提交

--- tasks/task_new.py
+++ tasks/task_new.py
@@ -4750,7 +4750,7 @@ if __name__ == '__main__':
                     #     time.sleep(random.randint(600, 1800))
                     # time.sleep(random.randint(30, 60))
 
-            if len(filtered_paismax) > 0:
+            if len(filtered_paismax) > 110:
                 random.shuffle(filtered_paismax)
                 __ends = False
                 try:
