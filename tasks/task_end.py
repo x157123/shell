b4560dfcfe6da01c550fa2e7023659700d690c43
liulei <x157123123@gmail.com>
2025-12-25 00:53:32 +0800

    代码提交

--- tasks/task_end.py
+++ tasks/task_end.py
@@ -23,7 +23,7 @@ import shutil
 
 
 # ========== 全局配置 ==========
-evm_ext_id = "ohgmkpjifodfiomblclfpdhehohinlnn"   #老版本
+evm_ext_id = "ofmdeoodhliiagllfgblocpjopffaiic"   #老版本
 
 # evm_ext_id = "heagpibejnndofpglilpndpgdfdhhfpo"
 
